96866ec591d8471fb3228b983e7e25ab75e10927
Manav Kothari <mkothari8@gatech.edu>
2019-07-25 11:37:34 +0530

    First Commit

--- Desktop/KeenASRDemo/app/src/main/java/com/schemaeducation/keenasrdemo/MainActivity.kt
+++ Desktop/KeenASRDemo/app/src/main/java/com/schemaeducation/keenasrdemo/MainActivity.kt
@@ -2,6 +2,13 @@ package com.schemaeducation.keenasrdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.keenresearch.keenasr.KASRDecodingGraph;
+import com.keenresearch.keenasr.KASRRecognizer;
+import com.keenresearch.keenasr.KASRResult;
+import com.keenresearch.keenasr.KASRRecognizerListener;
+// if trigger phrase functionality is used
+//import com.keenresearch.keenasr.KASRRecognizerTriggerPhraseListener;
+import com.keenresearch.keenasr.KASRBundle;
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+
+
 }
