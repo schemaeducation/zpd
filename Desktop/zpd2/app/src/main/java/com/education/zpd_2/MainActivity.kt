25037c794dbeabee0aaaeb38f4874eae5f748726
Manav Kothari <mkothari8@gatech.edu>
2019-12-17 09:29:34 +0530

    fetched JSON

--- Desktop/zpd2/app/src/main/java/com/education/zpd_2/MainActivity.kt
+++ Desktop/zpd2/app/src/main/java/com/education/zpd_2/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,23 @@ class MainActivity : AppCompatActivity() {
         //recyclerView_main.setBackgroundColor(Color.BLUE)
         recyclerView_main.layoutManager = LinearLayoutManager(this)
         recyclerView_main.adapter = MainAdapter()
+        fetchJSON()
+    }
 
+    fun fetchJSON(){
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed to execute")
+            }
 
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+            }
+        })
     }
 }
+
